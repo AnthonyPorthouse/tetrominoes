2c509bccbed30b95234a1fa8da0eab1482e9a99f
Anthony Porthouse <anthony@porthou.se>
2025-02-18 07:50:48 +0000

    fix: wrap entrypoint in async iife

--- src/main.ts
+++ src/main.ts
@@ -4,115 +4,118 @@ import "./style.css";
 import { buildUi } from "./ui";
 import { sounds } from "./ui/sounds";
 
-const app = new Application();
+(async () => {
+  const app = new Application();
 
-await app.init({ background: "#000", resizeTo: window });
-document.body.appendChild(app.canvas);
+  await app.init({ background: "#000", resizeTo: window });
+  document.body.appendChild(app.canvas);
 
-await Assets.load('tetromino.webp')
+  await Assets.load('tetromino.webp')
 
-sounds.bgm.play()
+  sounds.bgm.play()
 
 
-let game = new Game(10, 24)
+  let game = new Game(10, 24)
 
-let stepTimer = 0;
+  let stepTimer = 0;
 
-app.ticker.add((time) => {
+  app.ticker.add((time) => {
 
-  if (!app.stage.getChildByLabel('fps')) {
-    app.stage.addChild(new Text({
-      text: Math.round(time.FPS),
-      style: {
-        fill: 0xffffff,
-      },
-      label: 'fps'
-    }))
-  } else {
-    (app.stage.getChildByLabel('fps')! as Text).text = Math.round(time.FPS)
-  }
+    if (!app.stage.getChildByLabel('fps')) {
+      app.stage.addChild(new Text({
+        text: Math.round(time.FPS),
+        style: {
+          fill: 0xffffff,
+        },
+        label: 'fps'
+      }))
+    } else {
+      (app.stage.getChildByLabel('fps')! as Text).text = Math.round(time.FPS)
+    }
 
-  if (game.gamestate === 'playing') {
-    stepTimer = stepTimer % game.speed
-    stepTimer += 1 * time.elapsedMS;
+    if (game.gamestate === 'playing') {
+      stepTimer = stepTimer % game.speed
+      stepTimer += 1 * time.elapsedMS;
 
-    if (time.elapsedMS % 100) {
-      const clearedRows = game.state.clearRows();
-      switch(clearedRows) {
-        case 1:
-          game.score += 100;
-          break;
-        case 2:
-          game.score += 300;
-          break;
-        case 3:
-          game.score += 500;
-          break;
-        case 4:
-          game.score += 800;
-          break;
+      if (time.elapsedMS % 100) {
+        const clearedRows = game.state.clearRows();
+        switch(clearedRows) {
+          case 1:
+            game.score += 100;
+            break;
+          case 2:
+            game.score += 300;
+            break;
+          case 3:
+            game.score += 500;
+            break;
+          case 4:
+            game.score += 800;
+            break;
+        }
+
+        game.speed -= 10 * clearedRows
       }
 
-      game.speed -= 10 * clearedRows
-    }
-
-    if (stepTimer >= game.speed) {
-      game.state.moveTetrominoDown()
-    }
-  }
-
-  render(time, app, game)
-});
-
-window.addEventListener(
-  "keydown",
-  (e) => {
-    e.preventDefault();
-
-    if (game.gamestate == 'menu') {
-      if (e.key === 'Enter') {
-        game.gamestate = 'playing'
-        app.stage = new Container();
-        game.reset();
-        buildUi(app, game)
+      if (stepTimer >= game.speed) {
+        game.state.moveTetrominoDown()
       }
     }
 
-    if (game.gamestate == 'playing') {
-      if (e.key === 'Escape') {
-        game.gamestate = 'menu'
-        app.stage = new Container()
+    render(time, app, game)
+  });
+
+  window.addEventListener(
+    "keydown",
+    (e) => {
+      e.preventDefault();
+
+      if (game.gamestate == 'menu') {
+        if (e.key === 'Enter') {
+          game.gamestate = 'playing'
+          app.stage = new Container();
+          game.reset();
+          buildUi(app, game)
+        }
       }
 
-      if (e.key === "ArrowRight") {
-        game.state.moveTetrominoRight();
-      }
+      if (game.gamestate == 'playing') {
+        if (e.key === 'Escape') {
+          game.gamestate = 'menu'
+          app.stage = new Container()
+        }
 
-      if (e.key === "ArrowLeft") {
-        game.state.moveTetrominoLeft();
-      }
+        if (e.key === "ArrowRight") {
+          game.state.moveTetrominoRight();
+        }
 
-      if (e.key === "ArrowDown") {
-        stepTimer = 0;
-        game.state.moveTetrominoDown();
-      }
+        if (e.key === "ArrowLeft") {
+          game.state.moveTetrominoLeft();
+        }
 
-      if (e.key === "ArrowUp") {
-        stepTimer = 0;
-        let moved = true;
-        do {
-          moved = game.state.moveTetrominoDown();
-        } while(moved)
-      }
+        if (e.key === "ArrowDown") {
+          stepTimer = 0;
+          game.state.moveTetrominoDown();
+        }
 
-      if (e.key === 'd') {
-        game.state.attemptTetrominoRotateClockwise();
-      }
+        if (e.key === "ArrowUp") {
+          stepTimer = 0;
+          let moved = true;
+          do {
+            moved = game.state.moveTetrominoDown();
+          } while(moved)
+        }
 
-      if (e.key === 'a') {
-        game.state.attemptTetrominoRotateAntiClockwise();
+        if (e.key === 'd') {
+          game.state.attemptTetrominoRotateClockwise();
+        }
+
+        if (e.key === 'a') {
+          game.state.attemptTetrominoRotateAntiClockwise();
+        }
       }
-    }
-  },
-  { capture: true }
-);
+    },
+    { capture: true }
+  );
+
+})()
